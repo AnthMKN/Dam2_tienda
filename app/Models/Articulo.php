16e1ca29d2b6cb005b6c3d7024647224b048a717
jparloz <84689295+jparloz@users.noreply.github.com>
2023-01-28 18:08:39 +0100

    articulo modelo

--- app/Models/Articulo.php
+++ app/Models/Articulo.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class Articulo extends Model
 {
     use HasFactory;
+
+    protected $table = "articulos";
+
+    protected $fillable = [
+        'nombre',
+        'precio',
+        'stock',
+        'categoria',
+        'descripcion',
+        'fotos'
+    ];
+
+    protected $hidden = [
+        'id'
+        'id_proveedor'
+    ];
+
+    public function obtenerArticulos(){
+        return Articulos::all();
+    }
+
+    public function obtenerArticuloPorId($id){
+        return Articulo::find($id);
+    }
+
+    public function proveedor()
+    {
+        return $this->belongsTo(Proovedor::class, "id_proveedor", "id");
+    }
+
 }
